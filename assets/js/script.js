f3c95d694fb357b6487a66f35c37dcd9230fa04e
Dev007 <developer.ameen007@gmail.com>
2023-10-25 19:34:41 +0530

    feat(script.js): add getComputerChoice() function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 function cl(content) {
   console.log(content);
 }
+
+const choices = ['rock', 'paper', 'scissor'];
+
+function getComputerChoice() {
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  return computerChoice;
+}
