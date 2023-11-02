b452dab9c77ef1dca1c57ca44acfdf6b63e5c082
Dev007 <developer.ameen007@gmail.com>
2023-11-02 21:37:00 +0530

    feat(script.js): add getPlayerChoice() function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,18 @@ function getComputerChoice() {
   const computerChoice = choices[Math.floor(Math.random() * 3)];
   return computerChoice;
 }
+
+function getPlayerChoice() {
+
+  let playerChoice = prompt(`Rock, paper, or scissor. What's your choice?`);
+
+  playerChoice = playerChoice.toLowerCase();
+  
+  let validChoice = choices.indexOf(playerChoice)>-1 ? true : false;
+
+  if(validChoice) {
+    return [true, playerChoice];
+  }
+
+  return [false, playerChoice];
+}
