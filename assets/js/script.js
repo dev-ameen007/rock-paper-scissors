b07b31aad9e90760beb6763c56340f884484be58
Dev007 <developer.ameen007@gmail.com>
2023-11-06 10:49:01 +0530

    feat(script.js): add playRound() function

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,9 +17,41 @@ function getPlayerChoice() {
   
   let validChoice = choices.indexOf(playerChoice)>-1 ? true : false;
 
-  if(validChoice) {
-    return [true, playerChoice];
-  }
+  if(validChoice) return [true, playerChoice];
 
   return [false, playerChoice];
+}
+
+function playRound(playerSelectionDetailsArray, computerSelection) {
+  let winner; 
+  let message=``;
+
+  const playerSelectionInvalid = playerSelectionDetailsArray[0]==false ? true : false;
+  const playerSelection = playerSelectionDetailsArray[1];
+
+  if(playerSelectionInvalid) {
+    winner = '';
+    message = `Invalid input '${playerSelection}'. Please enter any of the following 'Rock', 'Paper', or 'Scissor'.`;
+    return [winner, message];
+  }
+
+  if(playerSelection===computerSelection) {
+    winner = 'draw';
+    message = `Whoooo!! Both choose ${computerSelection}, It's a tie!!`;
+    return [winner, message];
+  }
+
+  if(
+    playerSelection == 'rock' && computerSelection == 'paper' || 
+    playerSelection == 'scissor' && computerSelection == 'rock' || 
+    playerSelection == 'paper' && computerSelection == 'scissor'
+    ) {
+      winner = 'computer';
+      message = `Oops!! You Lose! ${computerSelection} beats ${playerSelection}.`
+      return [winner, message];
+  } else {
+    winner = 'player';
+    message = `Hooray!! You Won! ${playerSelection} beats ${computerSelection}.`
+    return [winner, message];
+  }
 }
