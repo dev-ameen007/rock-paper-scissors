ced4fef5a460190032c0dd88489863cae7931743
Dev007 <developer.ameen007@gmail.com>
2023-11-07 18:28:09 +0530

    refactor(script.js): enhance code efficiency
    - Enhanced the `getPlayerChoice` function to handle user cancellation
    of the input prompt and improved error handling.
    
    - In `playRound` function Replaced the multiple `if` statements with
    `switch` statement.
    
    - Removed the `resetGame` function and fix it by using a `while` loop
    
    - Replaced `for` loop with `while` loop in `game` function for improved
    control flow
    
    - Wrapped the main `game` function in a `try...catch` block to catch and
    handle any unexpected errors.
    
    - Added and improved comments for better code readability and
    maintainability.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,110 +1,108 @@
+// Shortcut to console.log(), simply use cl()
 function cl(content) {
   console.log(content);
 }
 
 const choices = ['rock', 'paper', 'scissor'];
 
+// Gets a random choice for the computer player from the 'choices' array
 function getComputerChoice() {
-  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  const computerChoice = choices[Math.floor(Math.random() * choices.length)];
   return computerChoice;
 }
 
+// Prompts the user for their choice and validates it against the 'choices' array
 function getPlayerChoice() {
 
-  let playerChoice = prompt(`Rock, paper, or scissor. What's your choice?`);
+  let playerChoice;
+  let notFirstPrompt = false;
 
-  playerChoice = playerChoice.toLowerCase();
-  
-  let validChoice = choices.indexOf(playerChoice)>-1 ? true : false;
+  do {
+    if(notFirstPrompt) alert(`Invalid input '${playerChoice}'.\nPlease enter any of the following 'Rock', 'Paper', or 'Scissor'.`);
 
-  if(validChoice) return [true, playerChoice];
+    playerChoice = prompt(`Rock, paper, or scissor. What's your choice?`);
 
-  return [false, playerChoice];
+    if(playerChoice === null) {
+      alert('Game cancelled!');
+      return playerChoice;
+    }
+
+    playerChoice = playerChoice.toLowerCase();
+    notFirstPrompt = true;
+  } while(!choices.includes(playerChoice));
+
+  return playerChoice;
 }
 
-function playRound(playerSelectionDetailsArray, computerSelection) {
-  let winner; 
+function playRound(playerSelection, computerSelection) {
+  let winner=''; 
   let message=``;
 
-  const playerSelectionInvalid = playerSelectionDetailsArray[0]==false ? true : false;
-  const playerSelection = playerSelectionDetailsArray[1];
-
-  if(playerSelectionInvalid) {
-    winner = '';
-    message = `Invalid input '${playerSelection}'. Please enter any of the following 'Rock', 'Paper', or 'Scissor'.`;
-    return [winner, message];
-  }
-
-  if(playerSelection===computerSelection) {
-    winner = 'draw';
-    message = `Whoooo!! Both choose ${computerSelection}, It's a tie!!`;
-    return [winner, message];
-  }
-
-  if(
-    playerSelection == 'rock' && computerSelection == 'paper' || 
-    playerSelection == 'scissor' && computerSelection == 'rock' || 
-    playerSelection == 'paper' && computerSelection == 'scissor'
-    ) {
+  switch(true) {
+    // Case where user cancel the prompt
+    case playerSelection === null:
+      winner = null;
+      break;
+    // Case where both players made the same choice
+    case playerSelection === computerSelection:
+      message = `Whoooo!! Both choose ${computerSelection}, It's a tie!!`;
+      break;
+    // Cases where the computer wins  
+    case playerSelection === 'rock' && computerSelection === 'paper' || 
+         playerSelection === 'scissor' && computerSelection === 'rock' || 
+         playerSelection === 'paper' && computerSelection === 'scissor':
       winner = 'computer';
-      message = `Oops!! You Lose! ${computerSelection} beats ${playerSelection}.`
-      return [winner, message];
-  } else {
-    winner = 'player';
-    message = `Hooray!! You Won! ${playerSelection} beats ${computerSelection}.`
-    return [winner, message];
+      message = `Oops!! You Lose! ${computerSelection} beats ${playerSelection}.`;
+      break;
+    // Default case where the player wins
+    default: 
+      winner = 'player';
+      message = `Hooray!! You Won! ${playerSelection} beats ${computerSelection}.`      
   }
+
+  return [winner, message];
 }
 
 const MAX_GAME_ROUNDS=5;
-let newGame=true;
-
-let gameRound=1;
-let playerWins=0;
-let computerWins=0;
-
-function resetGame() {
-  gameRound=1;
-  playerWins=0;
-  computerWins=0;
-}
 
 function game() {
-
-  if(newGame) resetGame();
-
-  for(let i=0;i<5;i++) {
-    newGame=false;
-
-    const playerChoice = getPlayerChoice();
-    const computerChoice = getComputerChoice();
-
-    const result = playRound(playerChoice, computerChoice);
-    const winner = result[0];
-    const message = result[1];
-
-    if(!winner) {
+  try {
+    let gameRound=1;
+    let computerScore=0;
+    let playerScore=0;
+  
+    // Game loop that runs for MAX_GAME_ROUNDS, updates scores based on the winner of each round, and logs the result of each round
+    while(gameRound <= MAX_GAME_ROUNDS) {
+  
+      const playerChoice = getPlayerChoice();
+      const computerChoice = getComputerChoice();
+  
+      const result = playRound(playerChoice, computerChoice);
+      const winner = result[0];
+      const message = result[1];
+  
+      // If player cancel the prompt, log a message and exit the game
+      if(winner === null) return 'Game Cancelled!';
+  
+      // updates scores based on the winner of each round, and logs the result of each round
+      if(winner==='computer') computerScore++;
+      if(winner==='player') playerScore++;
+  
       console.log(message);
-      game();
+  
+      gameRound++;
     }
-
-    if(winner=='computer') computerWins++;
-
-    if(winner=='player') playerWins++;
-
-    console.log(message);
-
-    if(gameRound==MAX_GAME_ROUNDS) break;  
-    gameRound++;
+  
+    // Determine the game winner based on the scores
+    const winnerAnnouncement = 
+      playerScore === computerScore ? `What a match, It's a tie!!! Try Again!!` :
+      playerScore > computerScore ? `Congrats!! You won the GAME!!!` :
+      `Oopsy, Computer won the game, better luck next time!!`;
+  
+    return `Game Over \n\nComputer Score: ${computerScore} \nPlayer Score: ${playerScore} \n\n${winnerAnnouncement}`;
+  } catch(error) {
+    console.error('An unexpected error occurred:', error);
   }
-
-  newGame=true;
-  const winnerAnnouncement = 
-    playerWins === computerWins ? `What a match, It's a tie!!! Try Again!!` :
-    playerWins > computerWins ? `Congrats!! You won the GAME!!!` :
-    `Oopsy, Computer won the game, better luck next time!!`;
-
-    return `Game Over \nComputer Points: ${computerWins} \nPlayer Points: ${playerWins} \n${winnerAnnouncement}`;
 }
 
 cl(game());
